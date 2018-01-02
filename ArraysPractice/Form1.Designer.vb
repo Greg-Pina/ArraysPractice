2034e2fa0b5d7b3d4ae70e75fb778f14bb0c1f84
Greg Pina <pinagm2825@gmail.com>
2018-01-02 05:28:03 -0500

    Initial commit

---
+++ ArraysPractice/Form1.Designer.vb
@@ -0,0 +1,115 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.btnSubmit = New System.Windows.Forms.Button()
+        Me.txtGrade = New System.Windows.Forms.TextBox()
+        Me.btnAverage = New System.Windows.Forms.Button()
+        Me.lblName = New System.Windows.Forms.Label()
+        Me.lblScore = New System.Windows.Forms.Label()
+        Me.cboName = New System.Windows.Forms.ComboBox()
+        Me.SuspendLayout()
+        '
+        'btnSubmit
+        '
+        Me.btnSubmit.Location = New System.Drawing.Point(31, 148)
+        Me.btnSubmit.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
+        Me.btnSubmit.Name = "btnSubmit"
+        Me.btnSubmit.Size = New System.Drawing.Size(56, 19)
+        Me.btnSubmit.TabIndex = 0
+        Me.btnSubmit.Text = "Submit"
+        Me.btnSubmit.UseVisualStyleBackColor = True
+        '
+        'txtGrade
+        '
+        Me.txtGrade.Location = New System.Drawing.Point(106, 84)
+        Me.txtGrade.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
+        Me.txtGrade.Name = "txtGrade"
+        Me.txtGrade.Size = New System.Drawing.Size(76, 20)
+        Me.txtGrade.TabIndex = 2
+        '
+        'btnAverage
+        '
+        Me.btnAverage.Location = New System.Drawing.Point(125, 148)
+        Me.btnAverage.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
+        Me.btnAverage.Name = "btnAverage"
+        Me.btnAverage.Size = New System.Drawing.Size(56, 19)
+        Me.btnAverage.TabIndex = 3
+        Me.btnAverage.Text = "Average"
+        Me.btnAverage.UseVisualStyleBackColor = True
+        '
+        'lblName
+        '
+        Me.lblName.AutoSize = True
+        Me.lblName.Location = New System.Drawing.Point(61, 39)
+        Me.lblName.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
+        Me.lblName.Name = "lblName"
+        Me.lblName.Size = New System.Drawing.Size(35, 13)
+        Me.lblName.TabIndex = 4
+        Me.lblName.Text = "Name"
+        '
+        'lblScore
+        '
+        Me.lblScore.AutoSize = True
+        Me.lblScore.Location = New System.Drawing.Point(61, 84)
+        Me.lblScore.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
+        Me.lblScore.Name = "lblScore"
+        Me.lblScore.Size = New System.Drawing.Size(35, 13)
+        Me.lblScore.TabIndex = 5
+        Me.lblScore.Text = "Score"
+        '
+        'cboName
+        '
+        Me.cboName.FormattingEnabled = True
+        Me.cboName.Items.AddRange(New Object() {"", "Ray", "Bob", "Pat"})
+        Me.cboName.Location = New System.Drawing.Point(106, 39)
+        Me.cboName.Name = "cboName"
+        Me.cboName.Size = New System.Drawing.Size(76, 21)
+        Me.cboName.TabIndex = 6
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(232, 206)
+        Me.Controls.Add(Me.cboName)
+        Me.Controls.Add(Me.lblScore)
+        Me.Controls.Add(Me.lblName)
+        Me.Controls.Add(Me.btnAverage)
+        Me.Controls.Add(Me.txtGrade)
+        Me.Controls.Add(Me.btnSubmit)
+        Me.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
+        Me.Name = "Form1"
+        Me.Text = "Ray, Bob and Pat"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents btnSubmit As System.Windows.Forms.Button
+    Friend WithEvents txtGrade As System.Windows.Forms.TextBox
+    Friend WithEvents btnAverage As System.Windows.Forms.Button
+    Friend WithEvents lblName As System.Windows.Forms.Label
+    Friend WithEvents lblScore As System.Windows.Forms.Label
+    Friend WithEvents cboName As System.Windows.Forms.ComboBox
+
+End Class
---
+++ ArraysPractice/Form1.vb
@@ -0,0 +1,83 @@
+﻿' **************************************************************************
+' <name>
+' <date>
+' This program shows how to use parrallel  arrays. There are 3 items in each
+' array storing a student's name, ther total grade and how many grades entered
+' This will allow calculation of an average grade
+' **************************************************************************
+
+Option Explicit On
+
+Public Class Form1
+
+   
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles btnSubmit.Click
+
+        Dim strName As String ' variable for students name
+        Dim dblGrade As Double ' variable for an entered grade
+
+        txtGrade.BackColor = Color.White
+
+        Try
+
+            If Validation(txtGrade.Text, dblGrade) = True Then
+
+               
+
+            End If
+
+        Catch ex As Exception
+            MessageBox.Show(ex.Message)
+        End Try
+
+    End Sub
+
+    Public Sub AddGrade(ByVal Grade As Double, ByVal Name As String)
+        ' add entered grade into array
+    
+
+    End Sub
+
+    Private Sub btnAverage_Click(sender As Object, e As EventArgs) Handles btnAverage.Click
+
+        ' figure out student's average
+        Dim strAverage As String
+
+     
+
+
+        Display(strAverage) ' pass the output string to the sub to display it
+
+    End Sub
+
+    Public Function Validation(ByVal Grade As String, ByRef GradeNumber As Double) As Boolean
+
+        ' validate grade is numeric
+        If IsNumeric(Grade) Then
+            GradeNumber = CDbl(Grade)
+        Else
+            MessageBox.Show("Please enter numbers only for grade.")
+            txtGrade.BackColor = Color.Yellow
+            txtGrade.Focus()
+            Return False ' return false if there is an issue
+        End If
+
+        ' validate a name is selected
+        If cboName.SelectedIndex = -1 Then
+            MessageBox.Show("Please select a name.")
+            Return False  ' return false if there is an issue
+        End If
+
+        ' return true if everything is ok
+        Return True
+
+    End Function
+
+    Public Sub Display(ByVal Output As String)
+
+        MessageBox.Show(Output)
+
+    End Sub
+
+End Class
